91b65d069c85c09d4f6173947cefe0edd91a52b3
Ionică Bizău <bizauionica@gmail.com>
2015-12-20 12:57:11 +0200

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,25 @@
+const findValue = require("../lib");
+
+// Some random object :)
+const obj = {
+    location: {
+        planet: "Mars"
+      , town: "Somewhere"
+    }
+  , names: [
+        { first: "Alice", age: 19 }
+      , { first: "Bob", age: 20 }
+    ]
+};
+
+console.log(findValue(obj, "location.planet"));
+// => "Mars"
+
+console.log(findValue(obj, "names"));
+// => [ { first: "Alice", age: 19 }, { first: "Bob", age: 20 } ]
+
+console.log(findValue(obj, "names.0.first"));
+// => "Alice"
+
+console.log(findValue(obj, "names.1.age"));
+// => 20
